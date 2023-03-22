21024900bf858c4026153e9d3118618b71f9e560
Ettore Hidoux <ettore.hidoux@imt-atlantique.net>
2023-03-22 09:36:53 +0100

    update report.py

--- Project/content/surfline/report/reports.py
+++ Project/content/surfline/report/reports.py
@@ -33,7 +33,7 @@ def get_small_report(request, spots, report_type):
             messages.append({"text": {"text": [text]}})
             if report["optimalScore"] == 2:
                 messages.append({"text": {"text": ["it s firin"]}})
-            forecast = "wind is {} ({}), with speed between {} and {}".format(
+            forecast = "wind is {} ({}), with speed between {} and {} km/h".format(
                 report["directionType"], int(report["direction"]), report["speed"], report["gust"])
             messages.append({"text": {"text": [forecast]}})
             
@@ -43,14 +43,14 @@ def get_small_report(request, spots, report_type):
             if report["optimalScore"] == 2:
                 messages.append({"text": {"text": ["it s firin"]}})
             forecast = "swell is {}m high, with {}s period".format(
-                report["height"], report["period"])
+                round(report["height"], 2), report["period"])
             messages.append({"text": {"text": [forecast]}})   
         
         if report_type == "weather":
             text = "so, i m getting the weather for today"
             messages.append({"text": {"text": [text]}})
             temperature = "air temperature is {}".format(report["temperature"])
-            condition = "it s {}".format(report["condition"])
+            condition = "it s {}".format(report["condition"].lower())
             messages.append({"text": {"text": [temperature]}})  
             messages.append({"text": {"text": [condition]}})  
             
@@ -85,35 +85,35 @@ def get_full_report(request, spots):
         
         # waves 
         waves = report.get_report("wave")
-        forecast = "waves are {}, i mean {}m".format(report["humanRelation"].lower(), report["min"])
+        forecast = "waves are {}, i mean {}m".format(waves["humanRelation"].lower(), waves["min"])
         messages.append({"text": {"text": [forecast]}})
         # wind 
         wind = report.get_report("wind")
-        forecast = "wind is {} ({}), with speed between {} and {}".format(report["directionType"], int(report["direction"]), report["speed"], report["gust"])
+        forecast = "wind is {} ({}), with speed between {} and {} km/h".format(wind["directionType"], int(wind["direction"]), wind["speed"], wind["gust"])
         messages.append({"text": {"text": [forecast]}})
         # weather
         weather = report.get_report("weather")
-        forecast = "air temperature is {} and weather is {}".format(weather["temperature"], weather["condition"])
+        forecast = "air temperature is {} and weather is {}".format(weather["temperature"], weather["condition"].lower())
         messages.append({"text": {"text": [forecast]}})  
         # tides 
         high = report.get_next_high_tide()
         low = report.get_next_low_tide()
         if high:
-            high_tides = "the next high tide is {}, {}m".format(high[0], high[1])
+            high_tides = "the next high tide is {}, {}m".format(high[0].strftime("%H:%M"), high[1])
             messages.append({"text": {"text": [high_tides]}})
         if low:
-            low_tides = "the next low tide is {}, {}m".format(low[0], low[1])
+            low_tides = "the next low tide is {}, {}m".format(low[0].strftime("%H:%M"), low[1])
             messages.append({"text": {"text": [low_tides]}})
         # sunlight
         now = datetime.now()
         sunlights = report.get_sunLights()
         if sunlights["sunset"] > now:
             print(sunlights["sunset"])
-            text = "the sunset will be at {}".format(sunlights["sunset"])
+            text = "the sunset will be at {}".format(sunlights["sunset"].strftime("%H:%M"))
             messages.append({"text": {"text": [text]}})
         if sunlights["dusk"] > now:
             print(sunlights["dusk"])
-            text = "the last light of sun today will be at {}".format(sunlights["dusk"])
+            text = "the last light of sun today will be at {}".format(sunlights["dusk"].strftime("%H:%M"))
             messages.append({"text": {"text": [text]}})
         
         if (waves["optimalScore"] == 2) & (wind["optimalScore"] == 2):
